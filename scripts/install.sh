7760cf75b92f57235d975e3c4a93876211d8f6c8
炒饭 <chaofan@2dfire.com>
2017-03-27 14:34:11 +0800

    fix

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-cd $(dirname $0)
+cd `dirname $0`/..
+
 cur_dir=$(pwd)
 dev_tools_config_dir="$HOME/.config/微信web开发者工具"
 
@@ -13,8 +14,7 @@ if [ -d "$dev_tools_config_dir" ]; then
   cd $dev_tools_config_dir/WeappVendor
   mkdir -p s
   mv wc* s
-  echo "cp $cur_dir/WeappVendor/* $(pwd)"
-  cp $cur_dir/WeappVendor/* ./
+  cp $cur_dir/scripts/WeappVendor/* ./
 fi
 
 
