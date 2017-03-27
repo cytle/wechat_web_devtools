13161d6dbd1d5e56a89eec0439998b7e82705fa6
炒饭 <chaofan@2dfire.com>
2017-03-27 15:23:46 +0800

    bug 清理

--- bin/pack.sh
+++ bin/pack.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 # tar wechat-dev-tools
 
+param1=$1
 cd `dirname $0`/..
 
 cur_dir=$(pwd)
@@ -17,10 +18,13 @@ nwjs_download="https://dl.nwjs.io/v$nwjs_v/nwjs-sdk-v${nwjs_v}-linux-x64.tar.gz"
 dist_wechat_dir="wechat-dev-tools-xsp"
 dist_wechat_package="wechat-v${wechat_v}-nwjs-v${nwjs_v}.tar.gz"
 
-
+if [ $param1 = "install" ]; then
+  # rm -rf $tmp_dir
+fi
 mkdir -p $tmp_dir
 if [ ! -d "$nwjs_dir" ]; then
   if [ ! -f "$nwjs_file" ]; then
+    echo "[注意]需要下载nwjs.请耐心等待下载完成"
     wget "$nwjs_download" -O $nwjs_file
     $? -ne 0 && exit "$?"
   fi
@@ -34,8 +38,15 @@ fi
 
 rm -rf $cur_dir/dist
 mkdir -p $cur_dir/dist
-cp -r "$nwjs_dir"/* "$cur_dir/package.nw" "$cur_dir/scripts" "$cur_dir/dist"
 
-mkdir -p $tmp_dir/build
-
-tar -zcvf "$tmp_dir/build/$dist_wechat_package" -C "$cur_dir" dist
+if [ $param1 = "install" ]; then
+  cp -r "$nwjs_dir"/* "$cur_dir/scripts" "$cur_dir/dist"
+  cd "$cur_dir/dist"
+  ln -s "$cur_dir/package.nw"
+  sh scripts/install.sh
+  # rm -rf $tmp_dir
+else
+  cp -r "$nwjs_dir"/* "$cur_dir/package.nw" "$cur_dir/scripts" "$cur_dir/dist"
+  mkdir -p $tmp_dir/build
+  tar -zcvf "$tmp_dir/build/$dist_wechat_package" -C "$cur_dir" dist
+fi
--- scripts/install.sh
+++ scripts/install.sh
@@ -7,14 +7,34 @@ dev_tools_config_dir="$HOME/.config/微信web开发者工具"
 
 rm -rf $dev_tools_config_dir
 
-./nw
 # TODO close nw
+./nw &
+nw_pid=$!
 
+echo "please wait 5s!"
+sleep 1s
+echo "4s"
+sleep 1s
+echo "3s"
+sleep 1s
+echo "2s"
+sleep 1s
+echo "1s"
+sleep 1s
+echo "after 5s"
+
+kill -9 $nw_pid
 if [ -d "$dev_tools_config_dir" ]; then
   cd $dev_tools_config_dir/WeappVendor
   mkdir -p s
   mv wc* s
+  echo "cp $cur_dir/scripts/WeappVendor/* $(pwd)"
   cp $cur_dir/scripts/WeappVendor/* ./
+  echo "install success!"
+else
+  echo "install faile! please reinstll"
 fi
+
+
 
 
