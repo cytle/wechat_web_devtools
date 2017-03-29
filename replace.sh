8995d181b6d8ac9fe1daa53afbff6c3c28d856da
炒饭 <chaofan@2dfire.com>
2017-03-29 11:35:42 +0800

    替换

--- replace.sh
+++ replace.sh
@@ -1,3 +1,7 @@
 #! /bin/sh
 
-find . -type f -name '*.js' | xargs sed
+files=find ./package.nw/app/dist -type f -name '*.js'
+
+$files | xargs sed -i 's/\.\/main\.js/.\/Main\.js/g'
+#find ./package.nw/app/dist -type f -name '*.js' | xargs sed -i 's/config\/appServiceConfig\.js/config\/appserviceConfig\.js/g'
+
