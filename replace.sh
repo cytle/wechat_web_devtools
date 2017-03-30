2f3cd7e977d10a44fbc1242d1d5c10b255e3f742
炒饭 <chaofan@2dfire.com>
2017-03-30 13:11:45 +0800

    替换大小写

--- replace.sh
+++ replace.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-files=find ./package.nw/app/dist -type f -name '*.js'
+files=$(find ./package.nw/app/dist -type f -name '*.js')
 
-$files | xargs sed -i 's/\.\/main\.js/.\/Main\.js/g'
-#find ./package.nw/app/dist -type f -name '*.js' | xargs sed -i 's/config\/appServiceConfig\.js/config\/appserviceConfig\.js/g'
+echo $files | xargs sed -i 's/\.\/main\.js/.\/Main\.js/g'
+echo $files | xargs sed -i 's/Create\/create/create\/create/g'
+echo $files | xargs sed -i 's/config\/appServiceConfig\.js/config\/appserviceConfig\.js/g'
 
