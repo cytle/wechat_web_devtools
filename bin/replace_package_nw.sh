9f6443fe0dbfc28c6fae11eeeccc2439a473ea04
炒饭 <chaofan@2dfire.com>
2017-05-29 17:39:13 +0800

    fix:sensor页面缺失

--- bin/replace_package_nw.sh
+++ bin/replace_package_nw.sh
@@ -2,5 +2,9 @@
 
 root_dir=$(cd `dirname $0`/.. && pwd -P)
 
-# 打开目录
+# 打开工具上方目录
 sed -ri -e 's/("win32")(\=\=\=process\.platform\|\|)(global\.appConfig\.isDev)/\1\2"linux"\2\3/g' "$root_dir/package.nw/app/dist/components/menubar/menubar.js"
+
+# 大小写错误
+# sensor工具
+sed -ri -e 's/Sensor\/index\.html/sensor\/index\.html/g' "$root_dir/package.nw/app/dist/extensions/devtools.html"
