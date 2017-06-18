9b1851d5a7788536817e0650d5c1cf44a1facd4b
炒饭 <chaofan@2dfire.com>
2017-06-18 16:55:44 +0800

    fix:ctrl+b 编译快捷键无效

--- bin/replace_package_nw.sh
+++ bin/replace_package_nw.sh
@@ -5,6 +5,10 @@ root_dir=$(cd `dirname $0`/.. && pwd -P)
 # 打开工具上方目录
 sed -ri -e 's/("win32")(\=\=\=process\.platform\|\|)(global\.appConfig\.isDev)/\1\2"linux"\2\3/g' "$root_dir/package.nw/app/dist/components/menubar/menubar.js"
 
-# 大小写错误
-# sensor工具
+# sensor工具 大小写错误
 sed -ri -e 's/Sensor\/index\.html/sensor\/index\.html/g' "$root_dir/package.nw/app/dist/extensions/devtools.html"
+
+# TODO certificateDetailsPromise
+
+# 绑定快捷键
+sed -ri -e 's/("win32"===process\.platform),/\1||"linux"===process\.platform,/g' "$root_dir/package.nw/app/dist/common/shortCut/shortCut.js"
