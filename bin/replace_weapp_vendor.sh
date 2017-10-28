769c8a3cd6cb5b1a023d57b167dc499bd704b7a7
炒饭 <chaofan@2dfire.com>
2017-10-28 15:49:45 +0800

    增加对没产生开发者工具配置文件夹的说明

--- bin/replace_weapp_vendor.sh
+++ bin/replace_weapp_vendor.sh
@@ -23,6 +23,7 @@ if [ -d "$dev_tools_config_dir" ]; then
   echo "cp -rfu $root_dir/bin/WeappVendor/* $dev_tools_config_dir/WeappVendor"
   cp -rf $root_dir/bin/WeappVendor/* "$dev_tools_config_dir/WeappVendor" 2> /dev/null
   echo "Success"
-else
+else\
+  echo "Error: 开发者工具未生成文件夹 $dev_tools_config_dir, 可能是权限问题, 请参考项目issue"
   echo "Fail! Please reinstall"
 fi
