cddaf33633659dbd845b515914061b4e92f99db2
炒饭 <chaofan@2dfire.com>
2018-02-04 23:13:42 +0800

    remove node相关

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var replace = require('gulp-replace');
-var path = require('path');
-
-gulp.task('desktop', function(){
-  gulp.src(['./desktop.example'])
-    .pipe(replace('bar', 'foo'))
-    .pipe(gulp.dest('dist/wechat_web_devtools.desktop'));
-});
