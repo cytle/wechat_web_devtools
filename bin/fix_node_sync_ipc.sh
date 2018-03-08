0e96590cc3f5951355b50867779c936db16961e0
炒饭 <chaofan@2dfire.com>
2018-03-09 01:34:13 +0800

    aaa

--- bin/fix_node_sync_ipc.sh
+++ bin/fix_node_sync_ipc.sh
@@ -3,5 +3,5 @@
 root_dir=$(cd `dirname $0`/.. && pwd -P)
 
 rm -rf $root_dir/package.nw/node_modules/node-sync-ipc
-cp -R $root_dir/packages/node-sync-ipc $root_dir/package.nw/node_modules
-ln -f $root_dir/bin/node $root_dir/dist
+ln -sf $root_dir/packages/node-sync-ipc $root_dir/package.nw/node_modules/node-sync-ipc
+ln -sf $root_dir/bin/node $root_dir/dist
