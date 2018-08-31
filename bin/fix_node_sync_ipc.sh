acdd4d905773cd1d9c31546226466de939fc5cb9
炒饭 <canyoutle@gmail.com>
2018-09-01 01:07:16 +0800

    style

--- bin/fix_node_sync_ipc.sh
+++ bin/fix_node_sync_ipc.sh
@@ -5,6 +5,8 @@ root_dir=$(cd `dirname $0`/.. && pwd -P)
 # rm -rf $root_dir/package.nw/node_modules/node-sync-ipc
 # ln -sf $root_dir/packages/node-sync-ipc $root_dir/package.nw/node_modules/node-sync-ipc
 
+ls $root_dir/package.nw/js/*.js | xargs sed -ri -e 's/f\.isMac\?"node-sync-ipc":"node-sync-ipc-nwjs"/"node-sync-ipc"/g'
+
 cd $root_dir/package.nw/node_modules/node-sync-ipc
 npm run install
 
--- bin/replace_package_nw.sh
+++ bin/replace_package_nw.sh
@@ -1,5 +1,3 @@
 #! /bin/bash
 
 root_dir=$(cd `dirname $0`/.. && pwd -P)
-
-ls $root_dir/package.nw/js/*.js | xargs sed -ri -e 's/f\.isMac\?"node-sync-ipc":"node-sync-ipc-nwjs"/"node-sync-ipc"/g'
