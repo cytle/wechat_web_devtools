3d76846089000618e9d201963575133f89f5f9a0
炒饭 <canyoutle@gmail.com>
2018-09-01 00:29:57 +0800

    fix node-ipc

--- bin/fix_node_sync_ipc.sh
+++ bin/fix_node_sync_ipc.sh
@@ -4,8 +4,6 @@ root_dir=$(cd `dirname $0`/.. && pwd -P)
 
 # rm -rf $root_dir/package.nw/node_modules/node-sync-ipc
 # ln -sf $root_dir/packages/node-sync-ipc $root_dir/package.nw/node_modules/node-sync-ipc
-ln -sf $root_dir/bin/node $root_dir/dist
-ln -sf $root_dir/bin/node $root_dir/dist/node.exe
 
 cd $root_dir/package.nw/node_modules/node-sync-ipc
 npm run install
