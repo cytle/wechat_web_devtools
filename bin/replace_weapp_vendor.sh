5353cff2670346febeb46065767cd21975dafe60
炒饭 <chaofan@2dfire.com>
2019-02-12 15:34:44 +0800

    chore: 脚本增加异常退出处理

--- bin/replace_weapp_vendor.sh
+++ bin/replace_weapp_vendor.sh
@@ -1,8 +1,37 @@
 #! /bin/bash
 
+set -e # 命令出错就退出
+trap 'catchError $LINENO $BASH_COMMAND' SIGHUP SIGINT SIGQUIT EXIT # 捕获错误情况
+
+catchError() {
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+        fail "\033[31mcommand: $2\n  at $0:$1\n  at $STEP\033[0m"
+    fi
+    exit $exit_code
+}
+
+success() {
+    echo -e "\033[42;37m 成功 \033[0m $1"
+}
+
+fail() {
+    echo -e "\033[41;37m 失败 \033[0m $1"
+}
+
+start_step() {
+    if [ -n "$STEP" ]; then success "$STEP"; fi
+    STEP="$1"
+    echo -e "\033[34m------------------------------------------------------------------\033[0m"
+    echo -e "\033[34m$STEP\033[0m"
+    echo -e "\033[34m------------------------------------------------------------------\033[0m"
+}
+
 root_dir=$(cd `dirname $0`/.. && pwd -P)
 
 dev_tools_config_dir="$HOME/.config/微信web开发者工具"
+
+start_step "生成"$dev_tools_config_dir
 
 if [ ! -d "$dev_tools_config_dir" ]; then
   cd $root_dir/dist;
@@ -22,8 +51,7 @@ fi
 if [ -d "$dev_tools_config_dir" ]; then
   echo "cp -rfu $root_dir/bin/WeappVendor/* $dev_tools_config_dir/WeappVendor"
   cp -rf $root_dir/bin/WeappVendor/* "$dev_tools_config_dir/WeappVendor" 2> /dev/null
-  echo "Success"
-else\
-  echo "Error: 开发者工具未生成文件夹 $dev_tools_config_dir, 可能是权限问题, 请参考项目issue"
-  echo "Fail! Please reinstall"
+  success "Success"
+else
+  success "开发者工具未生成文件夹 $dev_tools_config_dir, 可能是权限问题, 请参考项目issue"
 fi
