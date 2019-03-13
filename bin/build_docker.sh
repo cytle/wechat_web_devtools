d9dea388d1103e294be19e1d4462f8d9141a0535
炒饭 <chaofan@2dfire.com>
2019-03-13 14:25:02 +0800

    chore: update

--- bin/build_docker.sh
+++ bin/build_docker.sh
@@ -13,5 +13,26 @@ catchError() {
 
 cd "`dirname $0`/.."
 
+container_port=6081
+container_name="wxdt-update"
+container_image="canyoutle/wxdt:base"
+
+# kill wxdt-base
+docker kill $container_name || echo "$container_name is not running"
+# 运行容器
+docker run -d --rm \
+    --name $container_name -P -p $container_port:80 \
+    --mount type=bind,source=$PWD,target=/wxdt \
+    $container_image
+
+# 然后在浏览器中打开 http://ip:6080 `docker-machine ip`获得的ip
+echo "Please open this link(http://$(docker-machine ip):$container_port/) and continue after the installation"
+
+# 在容器内安装
+docker exec -it wxdt-base /wxdt/bin/update_package_nw.sh
+wechat_v=`cat ./wechat_v`
+
+echo $wechat_v
+
 # 构建dockerfile
 docker build -t canyoutle/wxdt .
--- bin/build_docker_base.sh
+++ bin/build_docker_base.sh
@@ -13,15 +13,25 @@ catchError() {
 
 cd "`dirname $0`/.."
 
+
+container_port=6080
+container_name=wxdt-update
+container_image="canyoutle/wxdt:prebase"
+
 # 构建dockerfile
-docker build -t canyoutle/wxdt-desktop:prebase -f Dockerfile-base .
+docker build -t $container_image -f Dockerfile-base .
 
 # kill wxdt-base
-docker kill wxdt-base || echo 'wxdt-base is not running'
+docker kill $container_name || echo "$container_name is not running"
 # 运行容器
-docker run -d --rm --name wxdt-base -P -p 6080:80 --mount type=bind,source=$PWD,target=/wxdt canyoutle/wxdt-desktop:prebase
+docker run -d --rm \
+    --name $container_name -P -p $container_port:80 \
+    --mount type=bind,source=$PWD,target=/wxdt \
+    $container_image
+
 # 然后在浏览器中打开 http://ip:6080 `docker-machine ip`获得的ip
-echo "Please open this link(http://$(docker-machine ip):6080/) and continue after the installation"
+echo "Please open this link(http://$(docker-machine ip):$container_port/) and continue after the installation"
+
 echo "sleep 10s"
 sleep 10s
 # 在容器内安装
